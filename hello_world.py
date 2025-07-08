7c8d8683e53edab7825816156c68bdd98c11c6f0
Jackalope35 <hrgarcia126@gmail.com>
2025-07-07 21:06:31 -0700

    added median function to median branch

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,10 @@
 greeting = "Hello!"
 print(greeting)
 
-def mean(a, b):
-    return (a+b)/2
+def median(a, b, c):
+    return sorted([a,b,c])[1]
 
 x=5
 y=10
-print(mean(x, y))
+z=7
+print(median(x, y,z))
