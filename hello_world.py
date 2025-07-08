e0f7c5f4b9a192d583d8b2440c009d3114456243
Jackalope35 <hrgarcia126@gmail.com>
2025-07-07 21:27:26 -0700

    restored function

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,12 @@
 greeting = "Hello!"
 print(greeting)
 
-def median(a, b, c):
-    return sorted([a,b,c])[1]
+greeting = "Hello!"
+print(greeting)
+
+def mean(a, b):
+    return (a+b)/2
 
 x=5
 y=10
-z=7
-print(median(x, y,z))
+print(mean(x, y))
